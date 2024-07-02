3b5f53bb7e4064ab88fc633fd6d84645d8f02ee4
charmilaS <charmilasimbine@gmail.com>
2024-07-02 14:44:41 +0200

    Changing template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,67 +1,16 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './vendor/tallstackui/tallstackui/src/**/*.php', 
-    ],
-
-    theme: {
-        extend: {
-            colors: {
-                'primary': {
-                    DEFAULT: '#2014ff',
-                    '50': '#2014ff',
-                    '100': '#2014ff',
-                    '200': '#2014ff',
-                    '300': '#2014ff',
-                    '400': '#2014ff',
-                    '500': '#2014ff',
-                    '600': '#2014ff',
-                    '700': '#2014ff',
-                    '800': '#2014ff',
-                    '900': '#2014ff',
-                    '950': '#2014ff',
-                },
-                'secondary': {
-                    DEFAULT: '#b5b5b5',
-                    '50': '#f7f7f7',
-                    '100': '#ededed',
-                    '200': '#dfdfdf',
-                    '300': '#c8c8c8',
-                    '400': '#b5b5b5',
-                    '500': '#999999',
-                    '600': '#888888',
-                    '700': '#7b7b7b',
-                    '800': '#676767',
-                    '900': '#545454',
-                    '950': '#363636',
-                },
-                'dark': {
-                    DEFAULT: '#3f4d69',
-                    '50': '#f6f7f9',
-                    '100': '#ebeef3',
-                    '200': '#d3d9e4',
-                    '300': '#acb8cd',
-                    '400': '#7f92b1',
-                    '500': '#5f7498',
-                    '600': '#4b5d7e',
-                    '700': '#3f4d69',
-                    '800': '#364156',
-                    '900': '#30384a',
-                    '950': '#202531',
-                }
-            },
-            
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+module.exports = {
+  content: ['./**/*.html', './dist/**/*.js'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ["'Inter'", 'sans-serif']
+      },
+      backgroundImage: {
+        'select-arrow': 'url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTExLjk5OTcgMTMuMTcxNEwxNi45NDk1IDguMjIxNjhMMTguMzYzNyA5LjYzNTg5TDExLjk5OTcgMTUuOTk5OUw1LjYzNTc0IDkuNjM1ODlMNy4wNDk5NiA4LjIyMTY4TDExLjk5OTcgMTMuMTcxNFoiIGZpbGw9InJnYmEoMTU2LDE2MywxNzUsMSkiPjwvcGF0aD48L3N2Zz4=")'
+      }
     },
+  },
+  plugins: [],
+}
 
-    plugins: [forms],
-};
