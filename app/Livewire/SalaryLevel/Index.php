1a2c8a77afd98a16a01a1b2c05040bcdd7255b83
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-11-24 16:25:06 +0200

    fix: fix salary level page

--- app/Livewire/SalaryLevel/Index.php
+++ app/Livewire/SalaryLevel/Index.php
@@ -3,13 +3,15 @@
 namespace App\Livewire\SalaryLevel;
 
 use App\Models\SalaryLevel;
-use Livewire\Attributes\Rule;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 use TallStackUi\Traits\Interactions;
 
 class Index extends Component
 {
     use Interactions;
+
+    public $showForm = false;
 
     public $id;
 
@@ -24,18 +26,20 @@ class Index extends Component
     // #[Rule('required', as: '"Nome"')]
     public $level;
 
-    protected function rules()
+    public function create()
     {
-        $rules = [
-            'level' => 'required|unique:salary_levels,level,'.$this->id,
-        ];
+        $this->reset();
+        $this->showForm = true;
 
-        return $rules;
     }
 
     public function store()
     {
-        $validated = $this->validate();
+        $validated = $this->validate(rules: [
+            'level' => ['required', 'numeric', Rule::unique(SalaryLevel::class, 'level')],
+        ], attributes: [
+            'level' => 'nível salarial',
+        ]);
         SalaryLevel::create($validated);
         $this->reset();
         $this->resetValidation();
@@ -48,11 +52,16 @@ class Index extends Component
         $query = SalaryLevel::findOrFail($id);
         $this->id = $id;
         $this->level = $query->level;
+        $this->showForm = true;
     }
 
     public function update()
     {
-        $validated = $this->validate();
+        $validated = $this->validate(rules: [
+            'level' => ['required', 'numeric', Rule::unique(SalaryLevel::class, 'level')->ignore($this->id)],
+        ], attributes: [
+            'level' => 'nível salarial',
+        ]);
         if ($this->id) {
             $query = SalaryLevel::findOrFail($this->id);
             $query->update($validated);
@@ -65,19 +74,11 @@ class Index extends Component
     public function deleteConfirm($id)
     {
         $this->id = $id;
-        $this->dialog()->confirm('Atenção!', 'Tem certeza que deseja eliminar?', [
-            'confirm' => [
-                'text' => 'Confirmar',
-                'method' => 'delete',
-                // 'params' => 'Confirmed Successfully' // Can be a string or array
-            ],
-            /* Cancel is optional */
-            'cancel' => [
-                'text' => 'Cancelar',
-                'method' => 'cancel',
-                // 'params' => 'Cancelled Successfully' // Can be a string or array
-            ],
-        ]);
+        $this->dialog()
+            ->question('Atenção!', 'Tem certeza que deseja eliminar?')
+            ->confirm('Confirmar', 'delete')
+            ->cancel('Cancelar', 'cancel')
+            ->send();
     }
 
     public function delete()
@@ -86,7 +87,7 @@ class Index extends Component
         $query->delete();
         $this->reset();
         $this->resetValidation();
-        $this->dialog()->success('Successo', 'Eliminado com Sucesso.');
+        $this->dialog()->success('Successo', 'Eliminado com Sucesso.')->send();
     }
 
     public function cancel()
--- resources/views/livewire/salary-level/index.blade.php
+++ resources/views/livewire/salary-level/index.blade.php
@@ -1,41 +1,43 @@
 <div class="m-0">
-    <div class="flex flex-col gap-6">
-        <x-card>
-            <x-slot:header>
-                ...
-            </x-slot:header>
-            <div class="p-6">
-                <form wire:submit="{{ $id ? 'update' : 'store' }}">
-                    <div class="grid gap-6 lg:grid-cols-3">
-                        <x-input wire:model="level" label="Nível do Salário *" />
+    @if ($showForm == true)
+        <div class="flex flex-col gap-6 mb-6">
+            <x-card>
+                <x-slot:header>
+                    {{ $id ? 'Atualizar' : 'Adicionar' }}
+                </x-slot:header>
+                <div class="p-6">
+                    <form wire:submit="{{ $id ? 'update' : 'store' }}">
+                        <div class="grid gap-6 lg:grid-cols-3">
+                            <x-input wire:model="level" label="Nível do Salário *" type="number" />
 
 
 
-                        <div class="mt-6 justify">
-                            <x-button md text="{{ $id ? 'Actualizar' : 'Salvar' }}" color="blue" />
-                            <x-button md wire:click="cancel" text="Cancelar" color="yellow" />
+                            <div class="mt-6 justify">
+                                <x-button type="submit" md text="{{ $id ? 'Actualizar' : 'Salvar' }}" color="blue" />
+                                <x-button md wire:click="cancel" text="Cancelar" color="yellow" />
+                            </div>
                         </div>
-                    </div>
 
-                </form>
+                    </form>
 
-            </div>
-        </x-card>
+                </div>
+            </x-card>
 
-        <!-- end card -->
+            <!-- end card -->
 
-    </div>
+        </div>
+    @endif
 
-    <div class="flex flex-col gap-6 mt-6">
+    <div class="flex flex-col gap-6x">
         <x-card>
             <x-slot:header>
                 <div class="flex flex-wrap items-center justify-between gap-1">
-                    <h4>...</h4>
-                    <div class="flex flex-wrap gap-1">
-                        <x-button md icon="plus" color="blue" text="Adicionar" />
-                    </div>
+                    <h4>Nível salarial</h4>
                 </div>
             </x-slot:header>
+            <div class="px-6 text-right justify">
+                <x-button md wire:click="create" icon="plus" text="Adicionar" color="blue" />
+            </div>
             <div class="p-6">
                 <div class="flex flex-wrap items-center justify-between py-2">
                 </div>
@@ -46,29 +48,27 @@
                         <tr>
                             <th
                                 class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200 text-center">
-                                ...</th>
+                                Id</th>
                             <th
                                 class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200 text-center">
                                 Nível do Salário</th>
                             <th
-                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200 hidden lg:table-cell">
-                                <div class="hidden lg:inline-block">Ações</div>
+                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200  lg:table-cell">
+                                <div class="lg:inline-block">Ações</div>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        @php $count = 1 @endphp
                         @foreach ($query as $key => $value)
                             <tr class="hover:bg-slate-50/50 dark:hover:bg-slate-800/70 dark:text-slate-300">
                                 <td
-                                    class="hidden px-2 py-1 text-sm font-light text-center border dark:border-slate-400 lg:table-cell">
-                                    {{ $count++ }}</td>
-                                <td
-                                    class="hidden px-2 py-1 text-sm font-light border dark:border-slate-400 lg:table-cell">
+                                    class="px-2 py-1 text-sm font-light text-center border dark:border-slate-400 lg:table-cell">
+                                    {{ $value->id }}</td>
+                                <td class="px-2 py-1 text-sm font-light border dark:border-slate-400 lg:table-cell">
                                     {{ $value->level }}</td>
                                 <td class="px-1 py-3 text-sm font-light text-center border dark:border-slate-400">
                                     <!-- Actions Desktop -->
-                                    <div class="items-center justify-center hidden gap-1 lg:flex">
+                                    <div class="items-center justify-center gap-1 lg:flex">
 
                                         {{-- @if (Auth::user()->can($title . '.edit')) --}}
                                         <x-button md wire:click='edit({{ $value->id }})' icon="pencil-square"
